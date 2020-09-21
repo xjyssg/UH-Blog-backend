5ee9d9c114975c46d1c2ab8e5fe584744f3d03c9
xjyssg <jiayuexue30@gmail.com>
2020-09-21 16:13:39 +0800

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = {
-  dummy
+const totalLikes = (blogs) => {
+  const reduceFunc = (sum, item) => {
+    return sum + item
+  }
+  return blogs.map(blog => blog.likes).reduce(reduceFunc, 0)
 }
+
+module.exports = {
+  dummy,
+  totalLikes
+}
+
